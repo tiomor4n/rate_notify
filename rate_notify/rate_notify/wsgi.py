6b197b22520eef7395b47c6d98044c18fed69d37
tiomor4n <tiomor4n@gmail.com>
2017-05-03 00:27:16 +0800

    init rate notify
    init rate notify

---
+++ rate_notify/rate_notify/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for rate_notify project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rate_notify.settings")
+
+application = get_wsgi_application()
+
+try:
+	from dj_static import Cling, MediaCling
+	application = Cling(get_wsgi_application())
+	application = Cling(MediaCling(get_wsgi_application()))
+except:
+	pass
