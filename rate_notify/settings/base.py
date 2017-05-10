1a375c692a9080b1d381cb201365276777f401ef
tiomor4n <tiomor4n@gmail.com>
2017-05-11 00:16:25 +0800

    a
    a

--- rate_notify/settings/base.py
+++ rate_notify/settings/base.py
@@ -115,7 +115,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
