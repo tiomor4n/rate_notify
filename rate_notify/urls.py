fa3d91a6a2576a37d2a1a2b1cfb97b361af7b73a
tiomor4n <tiomor4n@gmail.com>
2017-05-10 23:50:35 +0800

    change url

--- rate_notify/urls.py
+++ rate_notify/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from subscribe.views import index
+from subscribe.views import RunBatchOP
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^$', index),
 	url(r'^line_bot/', include('line_bot.urls')),
+	url(r'^runbatchop/', RunBatchOP), 
 ]
