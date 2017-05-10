680c591012b1b9a33fb491816e175495afb5e377
tiomor4n <tiomor4n@gmail.com>
2017-05-11 07:19:36 +0800

    a
    a

--- rate_notify/settings/base.py
+++ rate_notify/settings/base.py
@@ -123,6 +123,4 @@ STATICFILES_DIRS = (
 	#os.path.join(BASE_DIR, 'static'),
 )
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
